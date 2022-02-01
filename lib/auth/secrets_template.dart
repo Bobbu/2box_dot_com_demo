99e9e5fee66d3277eca0e64a907d6b041424cc28
Rob Daly <macbobbu@mac.com>
2022-02-01 09:16:27 -0500

    Improve secret instrucitons

--- lib/auth/secrets_template.dart
+++ lib/auth/secrets_template.dart
@@ -1,7 +1,8 @@
 class MyAppSecretsTEMPLATE {
-// 1) Remove "TEMPLATE" from the class name.
+  // 1) Rename this file secrets.dart
+  // 2) Remove "TEMPLATE" from the class name.
 
   // Box.com
-  static const boxClientId = '2) Put your box client id here';
-  static const boxClientSecret = '3) Put your box client secret here';
+  static const boxClientId = '3) Put your box client id here';
+  static const boxClientSecret = '4) Put your box client secret here';
 }
